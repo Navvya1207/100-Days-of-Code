b6c2a74596247e2196a70c6061c6796a07c43202
Navvya Bhardwaj <navvya.bhardwaj12jul@gmail.com>
2025-09-01 00:34:02 +0530

    Update and rename Q20.c to Qu20.c

--- Qu20.c
+++ Qu20.c
@@ -1,4 +1,4 @@
-/* Q20: Write a program to display the day of the week based on a number (1–7) using switch-case.
+/* Q20: Write a program to display the day of the week based on a number (1â€“7) using switch-case.
 
 /*
 Sample Test Cases:
