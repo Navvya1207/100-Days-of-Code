9626a064dd53c55c988ce1f69c5c5cac9862f877
Navvya Bhardwaj <navvya.bhardwaj12jul@gmail.com>
2025-09-01 00:45:14 +0530

    Update Qu20.c

--- Qu20.c
+++ Qu20.c
@@ -1,6 +1,4 @@
 /* Q20: Write a program to display the day of the week based on a number (1–7) using switch-case.
-
-/*
 Sample Test Cases:
 Input 1:
 1
@@ -11,7 +9,6 @@ Input 2:
 5
 Output 2:
 Friday
-
 */
 #include <stdio.h>
 int main(){
@@ -31,3 +28,4 @@ int main(){
 	return 0;
 	
 }
+
