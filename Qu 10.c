d3d0467ea38c05d9a489ba4355cc27302410d85c
Navvya Bhardwaj <navvya.bhardwaj12jul@gmail.com>
2025-09-01 00:39:06 +0530

    Update Qu 10.c

--- Qu 10.c
+++ Qu 10.c
@@ -1,6 +1,4 @@
 /*Q10: Write a program to input time in seconds and convert it to hours:minutes:seconds format.
-
-/*
 Sample Test Cases:
 Input 1:
 3661
@@ -11,7 +9,6 @@ Input 2:
 7322
 Output 2:
 2:2:2
-
 */
 #include <stdio.h>
 int main(){
@@ -24,3 +21,4 @@ int main(){
 	printf("%d:%d:%d",h,m,s);
 	return 0;
 }
+
