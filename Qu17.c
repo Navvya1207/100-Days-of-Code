ab30bd81de538701f5a3ceef3b5bf13c006e157b
Navvya Bhardwaj <navvya.bhardwaj12jul@gmail.com>
2025-09-01 00:44:23 +0530

    Update Qu17.c

--- Qu17.c
+++ Qu17.c
@@ -1,6 +1,4 @@
 /* Q17: Write a program to find the roots of a quadratic equation and categorize them.
-
-/*
 Sample Test Cases:
 Input 1:
 1 -3 2
@@ -16,7 +14,6 @@ Input 3:
 1 2 5
 Output 3:
 Roots are complex
-
 */
 #include <stdio.h>
 #include <math.h>
@@ -42,3 +39,4 @@ int main(){
     printf("Roots are complex");
 	return 0;
 }
+
